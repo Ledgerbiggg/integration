a441523ec61a8128b2aea6887fa9748c0da2de41
ledger <228664584@qq.com>
2024-04-03 15:56:27 +0800

    这个是基础版本但是还是有问题啊,增加了注释什么的

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import logging
-
+from .switch import MyDeviceSwitch
+from homeassistant.helpers.entity_platform import async_get_platforms
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
@@ -11,14 +12,19 @@ _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass: HomeAssistant, hass_config):
-    # 添加开关
-    hass.helpers.discovery.load_platform('switch', DOMAIN, {}, hass_config)
+    """
+    这个函数是所有HomeAssistant集成的初始化函数，在HomeAssistant启动或重新加载配置时调用。
+    函数执行一次。
+    :param hass: HomeAssistant的实例，提供了与HomeAssistant系统进行交互的函数和属性。
+    :param hass_config:  HomeAssistant的配置信息，包含了所有配置文件中的数据。
+    :return: 初始化成功则返回True，否则返回False
+    """
 
     # 添加服务
     async def handle_my_service(call):
-        # Get the service data
+        # 服务需要传入参数
         url = call.data.get('url',
-                            'http://192.168.140.58:8000/user')
+                            'http://192.168.31.55:8000')
         username = call.data.get('username', 'ledger')
         # 添加参数
         params = {'username': username}
@@ -28,30 +34,51 @@ async def async_setup(hass: HomeAssistant, hass_config):
             response_text = response.text()
             _LOGGER.info(response_text)
 
-    hass.services.async_register(DOMAIN, 'my_service', handle_my_service)
-
-    # 添加设备
-    my_light = MyLight()
-    hass.data.setdefault("hello_state", []).append(my_light)
+    # # 添加设备
+    # my_light = MyLight()
+    # hass.data.setdefault("hello_state", []).append(my_light)
+    # async_add_entities([my_light], update_before_add=True)
     # 将设备绑定到集成上面
-    hass.helpers.discovery.load_platform("light", "hello_state", {}, hass_config)
 
+    # 添加开关
+    hass.helpers.discovery.load_platform('switch', "hello_state", {}, hass_config)
+    # 添加服务
+    hass.services.async_register(DOMAIN, 'my_service', handle_my_service)
+    # 添加灯光
+    hass.helpers.discovery.load_platform("light", "hello_state", {}, hass_config)
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "light"))
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "switch"))
+    """
+    这个函数在添加或更新每一个集成的配置入口时被调用。
+    对于每一个配置入口，函数将执行一次。
+    :param hass: HomeAssistant的实例，提供了与HomeAssistant系统进行交互的函数和属性。
+    :param entry: 代表单个集成的配置入口, 包含集成的配置数据。
+    :return: 如果配置入口成功地设置了集成,则返回True, 否则返回False.
+    """
+    """设置设备和实体"""
+
+    # 在这里你可以创建和存储设备实例，它可以是一个智能开关、传感器或任何其他设备
+    # device = MyDeviceSwitch(entry.data['name'])
+
+    # 创建并初始化开关实体
+    # async def async_add_device_entities():
+    #     """添加和注册这个设备相关的实体。"""
+    #     # 获取开关平台
+    #     platform = await async_get_platforms(hass, DOMAIN)
+    #     sender = util.RequestSender()
+    #     sender.send_request()
+    #
+    #     # 创建开关实体。这应该是你在switch模块定义的实体类
+    #     my_switch_entity = MyDeviceSwitch("newSwitch")
+    #
+    #     # 如果你的实体还有其它的设置，可以在此处配置
+    #
+    #     # 添加实体
+    #     if platform:
+    #         platform[0].async_add_entities([my_switch_entity])
+    #
+    # hass.async_create_task(async_add_device_entities())
+
     return True
-
-
-# async def async_update_options(hass: HomeAssistant, entry: ConfigEntry):
-#     return
-#
-#
-# async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-#     return
-#
-#
-# async def async_add_setuper(hass: HomeAssistant, config, domain, setuper):
-#     return
--- config_flow.py
+++ config_flow.py
@@ -6,13 +6,28 @@ DOMAIN = "hello_state"
 ROOMS = ["卧室", "厨房"]  # 列出所有可能的房间
 
 
-async def async_setup_entry(hass, entry):
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "switch"))
-    return True
+# async def async_setup_entry(hass, entry):
+#     """
+#     这个函数是在你的集成被用户添加到 Home Assistant 后执行的。它的主要任务是设置集成，并初始化你的设备和服务。
+#     :param hass:  这是一个指向当前 Home Assistant 运行实例的引用。
+#     :param entry: 这是一个 ConfigEntry 对象，包含用户在设置集成时提供的数据（你在配置流程中收集的数据）。
+#     :return:
+#     """
+#     hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "switch"))
+#     return True
 
 
 # 添加集成的时候提示词语
 class HelloStateConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+    # @staticmethod
+    # @callback
+    # def async_get_options_flow(
+    #         config_entry: config_entries.ConfigEntry,
+    # ) -> config_entries.OptionsFlow:
+    #     """Create the options flow."""
+    #     return OptionsFlowHandler(config_entry)
+
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             await self.async_set_unique_id(user_input['name'])
@@ -30,3 +45,27 @@ class HelloStateConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema({vol.Required("name"): str}),
             errors=errors or {},
         )
+
+# class OptionsFlowHandler(config_entries.OptionsFlow):
+#     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+#         """Initialize options flow."""
+#         self.config_entry = config_entry
+#
+#     async def async_step_init(
+#         self, user_input: dict[str, Any] | None = None
+#     ) -> FlowResult:
+#         """Manage the options."""
+#         if user_input is not None:
+#             return self.async_create_entry(title="", data=user_input)
+#
+#         return self.async_show_form(
+#             step_id="init",
+#             data_schema=vol.Schema(
+#                 {
+#                     vol.Required(
+#                         "show_things",
+#                         default=self.config_entry.options.get("show_things"),
+#                     ): bool
+#                 }
+#             ),
+#         )
